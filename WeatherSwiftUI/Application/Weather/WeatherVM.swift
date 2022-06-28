c707b45787024cf3b314c0d783474ac6a407a7c5
Alaattin Bedir <alaattin.bedir@intertech.com.tr>
2022-06-28 10:27:53 +0300

    amend

--- WeatherSwiftUI/Application/Weather/WeatherVM.swift
+++ WeatherSwiftUI/Application/Weather/WeatherVM.swift
@@ -9,10 +9,6 @@ import Foundation
 import MLBasicKit
 import Combine
 
-enum WeatherViewState: ViewState {
-    case close (String)
-}
-
 class WeatherVM: BaseVM {
     @Published var cityName: String = "Barcelona"
     @Published var weatherType: String? = nil
@@ -20,10 +16,6 @@ class WeatherVM: BaseVM {
     @Published var currentDate: Int? = nil
     @Published var weather: WeatherResponse? = nil    
     var currentLocation: (latitude:Double, longitude:Double) = (41.3874, 2.1686)
-
-    func closeButtonPressed() {
-        state.send(WeatherViewState.close("Closed"))
-    }
 }
 
 extension WeatherVM {
@@ -38,8 +30,6 @@ extension WeatherVM {
             self.currentDate = weather.currently?.time
             self.weatherType = weather.currently?.summary
             self.currentCityTemp = weather.currently?.temperature
-
-            self.closeButtonPressed()
 
         }, failed: { (error) in
             print(error)
