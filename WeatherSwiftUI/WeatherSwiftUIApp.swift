1cfb924839fea3cdb044956d1c3d292a617c3e22
Alaattin Bedir <alaattin.bedir@intertech.com.tr>
2022-06-28 09:47:29 +0300

    Initial Commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Alaattin Bedir on 28.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
