c0065303b4497ce4980748dd42200f66f2664953
Alaattin Bedir <alaattin.bedir@intertech.com.tr>
2022-06-30 09:54:22 +0300

    amend

--- WeatherSwiftUI/Presentation/Weather/WeatherVM.swift
+++ WeatherSwiftUI/Presentation/Weather/WeatherVM.swift
@@ -18,20 +18,20 @@ class WeatherVM: BaseVM {
     @Published var currentDate: String? = nil
     @Published var responseDailyList: [ResponseData]? = [ResponseData]()
     @Published var responseHourlyList: [ResponseData]? = [ResponseData]()
-
+    
     var currentLocation: (latitude:Double, longitude:Double) = (41.3874, 2.1686)
-
+    
     private let weatherApi: WeatherDataSource
-
+    
     required convenience init() {
         self.init(weatherApi: WeatherAPI())
     }
-
+    
     nonisolated init (weatherApi: WeatherDataSource = WeatherAPI()) {
         self.weatherApi = weatherApi
         super.init()
     }
-
+    
     func onAppear() {
         // Set the default to clear
         UITableView.appearance().backgroundColor = .clear
@@ -40,19 +40,19 @@ class WeatherVM: BaseVM {
 
 extension WeatherVM {
     func fetchCurrentWeather() {
-
+        
         // Get current weather
         weatherApi.fetchWeather(latitude: currentLocation.latitude,
-                                  longitude: currentLocation.longitude,
-                                   succeed: { [weak self] (weather) in
+                                longitude: currentLocation.longitude,
+                                succeed: { [weak self] (weather) in
             guard let self = self else { return }
-
-            self.currentDate = weather.currently?.time.map{ Helper.sharedInstance.getFormatedDate(date: Double(($0))) }
+            
+            self.currentDate = weather.currently?.time.map { Helper.sharedInstance.getFormatedDate(date: Double(($0))) }
             self.weatherType = weather.currently?.summary
             self.currentCityTemp = weather.currently?.temperature.map { Helper.sharedInstance.convertFahrenheitToCelsius(fahrenheit:$0) }
             self.responseDailyList = weather.daily?.data
             self.responseHourlyList = weather.hourly?.data
-
+            
         }, failed: { (error) in
             print(error)
         })
