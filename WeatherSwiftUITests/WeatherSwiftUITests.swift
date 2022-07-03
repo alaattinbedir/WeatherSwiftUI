f6ca1af548e4ffcbc52535e0a8079834460394b5
Alaattin Bedir <alaattin.bedir@intertech.com.tr>
2022-07-03 10:49:44 +0300

    added unit test

--- WeatherSwiftUITests/WeatherSwiftUITests.swift
+++ WeatherSwiftUITests/WeatherSwiftUITests.swift
@@ -6,31 +6,34 @@
 //
 
 import XCTest
+import MLNetworking
 @testable import WeatherSwiftUI
+
+
+class MockWeatherApiService: WeatherApiServiceProtocol {
+    func fetchWeather(latitude: Double, longitude: Double, succeed: @escaping (WeatherModel) -> Void, failed: @escaping (ErrorMessage) -> Void) {
+        succeed(WeatherModel(weatherType: "Clear", currentCityTemp: 79.77, currentDate: 1656784865, responseDailyList: [], responseHourlyList: []))
+    }
+}
 
 class WeatherSwiftUITests: XCTestCase {
 
+    var sut: WeatherVM!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = WeatherVM(apiService: MockWeatherApiService())
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    @MainActor func test_fetchWeather() throws {
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+        XCTAssert(sut.weatherType == "")
 
+        sut.fetchCurrentWeather()
+
+        XCTAssert(sut.weatherType == "Clear")
+    }    
 }
