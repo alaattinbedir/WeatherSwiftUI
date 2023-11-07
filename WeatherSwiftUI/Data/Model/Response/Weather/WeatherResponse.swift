324866a4c59c4d8e588e8bcd4616e78a26c50445
Alaattin Bedir <alaattinbedir@hotmail.com>
2023-11-07 15:48:43 +0300

    fix model

--- WeatherSwiftUI/Data/Model/Response/Weather/WeatherResponse.swift
+++ WeatherSwiftUI/Data/Model/Response/Weather/WeatherResponse.swift
@@ -35,27 +35,40 @@ struct WeatherResponse: Codable {
 // MARK: - Current
 struct Current: Codable {
     let dt: Int
+    let temp: Double
+    let humidity: Int
     let sunrise, sunset: Int?
-    let temp, feelsLike: Double
-    let pressure, humidity: Int
-    let dewPoint, uvi: Double
-    let clouds, visibility: Int
-    let windSpeed: Double
+    let uvi: Double
     let windDeg: Int
     let weather: [Weather]
+    let feelsLike: Double
+    let clouds, visibility: Int
+    let windSpeed: Double
+    let pressure: Int
+    let dewPoint: Double
     let windGust, pop: Double?
+    let rain: Rain?
 
     enum CodingKeys: String, CodingKey {
-        case dt, sunrise, sunset, temp
-        case feelsLike = "feels_like"
-        case pressure, humidity
-        case dewPoint = "dew_point"
-        case uvi, clouds, visibility
-        case windSpeed = "wind_speed"
+        case dt, temp, humidity, sunrise, sunset, uvi
         case windDeg = "wind_deg"
         case weather
+        case feelsLike = "feels_like"
+        case clouds, visibility
+        case windSpeed = "wind_speed"
+        case pressure
+        case dewPoint = "dew_point"
         case windGust = "wind_gust"
-        case pop
+        case pop, rain
+    }
+}
+
+// MARK: - Rain
+struct Rain: Codable {
+    let the1H: Double
+
+    enum CodingKeys: String, CodingKey {
+        case the1H = "1h"
     }
 }
 
@@ -63,8 +76,8 @@ struct Current: Codable {
 struct Weather: Codable {
     let id: Int
     let main: Main
-    let description: Description
     let icon: String
+    let description: Description
 }
 
 enum Description: String, Codable {
@@ -72,6 +85,7 @@ enum Description: String, Codable {
     case clearSky = "clear sky"
     case fewClouds = "few clouds"
     case lightRain = "light rain"
+    case moderateRain = "moderate rain"
     case overcastClouds = "overcast clouds"
     case scatteredClouds = "scattered clouds"
 }
@@ -84,41 +98,44 @@ enum Main: String, Codable {
 
 // MARK: - Daily
 struct Daily: Codable {
-    let dt, sunrise, sunset, moonrise: Int
-    let moonset: Int
-    let moonPhase: Double
+    let pop: Double
+    let rain: Double?
+    let dt: Int
     let temp: Temp
-    let feelsLike: FeelsLike
-    let pressure, humidity: Int
-    let dewPoint, windSpeed: Double
+    let humidity, sunrise, sunset: Int
+    let uvi, moonPhase: Double
     let windDeg: Int
     let windGust: Double
+    let moonset: Int
+    let feelsLike: FeelsLike
     let weather: [Weather]
+    let windSpeed: Double
+    let pressure, moonrise: Int
+    let dewPoint: Double
     let clouds: Int
-    let pop, uvi: Double
-    let rain: Double?
 
     enum CodingKeys: String, CodingKey {
-        case dt, sunrise, sunset, moonrise, moonset
+        case pop, rain, dt, temp, humidity, sunrise, sunset, uvi
         case moonPhase = "moon_phase"
-        case temp
-        case feelsLike = "feels_like"
-        case pressure, humidity
-        case dewPoint = "dew_point"
-        case windSpeed = "wind_speed"
         case windDeg = "wind_deg"
         case windGust = "wind_gust"
-        case weather, clouds, pop, uvi, rain
+        case moonset
+        case feelsLike = "feels_like"
+        case weather
+        case windSpeed = "wind_speed"
+        case pressure, moonrise
+        case dewPoint = "dew_point"
+        case clouds
     }
 }
 
 // MARK: - FeelsLike
 struct FeelsLike: Codable {
-    let day, night, eve, morn: Double
+    let night, eve, day, morn: Double
 }
 
 // MARK: - Temp
 struct Temp: Codable {
-    let day, min, max, night: Double
-    let eve, morn: Double
+    let night, min, eve, day: Double
+    let max, morn: Double
 }
